02cfea24fe05883c9e604af92c15bfa5b7c51163
ITQ <itq.dev@ya.ru>
2024-02-04 18:27:01 +0300

    Added Vercel Analytics and Vercel Speed Insights

--- app/layout.js
+++ app/layout.js
@@ -1,6 +1,9 @@
 import { Header } from "./components/Header/Header"
 import { Footer } from "./components/Footer/Footer"
 
+import { Analytics } from '@vercel/analytics/react';
+import { SpeedInsights } from "@vercel/speed-insights/next"
+
 import "./globals.css"
 
 export const metadata = {
@@ -15,6 +18,8 @@ export default function RootLayout({ children }) {
 				<Header />
 				{children}
 				<Footer />
+				<Analytics />
+				<SpeedInsights />
 			</body>
 		</html>
 	)
